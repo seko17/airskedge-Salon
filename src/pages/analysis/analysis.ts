9ce96fcfcba3b1ff5f92c9bd937641c9fa81c403
seko17 <34337236+seko17@users.noreply.github.com>
2019-09-12 09:09:01 +0200

    seko

---
+++ src/pages/analysis/analysis.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ViewReviewsPage } from '../view-reviews/view-reviews';
+
+/**
+ * Generated class for the AnalysisPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-analysis',
+  templateUrl: 'analysis.html',
+})
+export class AnalysisPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AnalysisPage');
+  }
+  reviews(){
+    this.navCtrl.push(ViewReviewsPage);
+  }
+
+}
