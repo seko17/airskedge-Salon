dc493052690734bd10978548de8732d82ffd15aa
ZaneleNkosi <50693705+ZaneleNkosi@users.noreply.github.com>
2019-09-19 10:21:07 +0200

    hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,7 +49,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       //private screenOrientation: ScreenOrientation, 
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.styleLightContent();
       setTimeout(()=>{
         splashScreen.hide();
       }, 1000);
