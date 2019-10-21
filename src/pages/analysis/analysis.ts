da1908b7afa19de3fd6f5b18f048c0dd29e4d9dc
Nathanael <50619296+NathanaelZondo@users.noreply.github.com>
2019-10-21 14:23:49 +0200

    c

--- src/pages/analysis/analysis.ts
+++ src/pages/analysis/analysis.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides, ViewController } from 'ionic-angular';
 import { ViewReviewsPage } from '../view-reviews/view-reviews';
 import { BookingsPage } from '../bookings/bookings';
+
 /**
  * Generated class for the AnalysisPage page.
  *
@@ -15,8 +16,8 @@ import { BookingsPage } from '../bookings/bookings';
   templateUrl: 'analysis.html',
 })
 export class AnalysisPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('slides') slides: Slides;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public vw:ViewController) {
   }
 
   ionViewDidLoad() {
@@ -27,7 +28,31 @@ export class AnalysisPage {
   }
 
   goToHome(){
-    this.navCtrl.push(BookingsPage);
+    this.vw.dismiss();
+  }
+  
+  hidden:boolean =false;
+  ionViewDidEnter()
+  {
+    this.hidden =false;
+  }
+
+  slideState() {
+  
+    console.log("Index = ",this.slides._activeIndex)
+    if(this.slides._activeIndex == 2)
+    {
+      this.hidden =true;
+    }
+  }
+
+  nextState() {
+    this.slides.slideNext();
+    console.log("Index = ",this.slides._activeIndex)
+    if(this.slides._activeIndex == 2)
+    {
+      this.hidden =true;
+    }
   }
 
 }
